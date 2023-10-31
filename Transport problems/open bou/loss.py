4d4259df3c06b2db6d7612c7e6d147afc4d56f49
double110 <54135207+double110@users.noreply.github.com>
2023-10-31 16:48:01 +0800

    Update loss.py

--- Transport problems/open bou/loss.py
+++ Transport problems/open bou/loss.py
@@ -63,7 +63,7 @@ def T_loss(Tmodel,x,y,t,dt,k,T1,T0,dTa,dTc,h,ha,hc,ddhTdd,dhdx,dhdy):
         #dTa=dTa+dt*k*ddhTdd
         #dTc=dTc-dt*k*ddhTdd
 
-    l1=loss1(Tb,T0)/(dt**2)*10
+    l1=loss1(Tb,T0)/(dt**2)*20
     l2=loss2(Ta,(dTa+h*T0)/ha)/(dt**2)
     l3=loss3(Tc,(dTc+h*T0)/hc)/(dt**2)
     return l1+l2+l3
